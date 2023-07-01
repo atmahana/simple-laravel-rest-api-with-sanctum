619eed3ac6d30972118ec1e42565628888c01479
Adham <zubairadham.ar@gmail.com>
2023-07-01 17:32:18 +0800

    init

---
+++ app/Filters/V1/InvoicesFilter.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Filters\V1;
+
+use Illuminate\Http\Request;
+use App\Filters\ApiFilter;
+
+class InvoicesFilter extends ApiFilter
+{
+    protected $safeParms = [
+        'customerId' => ['eq'],
+        'amount' => ['eq', 'gt', 'lt', 'gte', 'lte'],
+        'status' => ['eq', 'ne'],
+        'billed_date' => ['eq', 'gt', 'lt', 'gte', 'lte'],
+        'paid_date' => ['eq', 'gt', 'lt', 'gte', 'lte'],
+    ];
+
+    protected $columnMap = [
+        'customerId' => 'customer_id',
+        'billedDDate' => 'billed_date',
+        'paidDate' => 'paid_date',
+    ];
+
+    protected $operatorMap = [
+        'eq' => '=',
+        'lt' => '<',
+        'lte' => '<=',
+        'gt' => '>',
+        'gte' => '>=',
+        'ne' => '!=',
+    ];
+}
